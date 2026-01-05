0eedc919ab3a9cc9b9632d3e80b8d4a987b39cf7
Borys Kubisty <26774196+cpu-byte@users.noreply.github.com>
2026-01-05 18:07:36 +0000

    feat: create teamviewer's observe list retrieval

---
+++ src/db/teamviewer-observe-list.ts
@@ -0,0 +1,19 @@
+import {
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "../firebase-client";
+
+export async function getTeamViewerObserveList() {
+  const docRef = doc(db, "config", "teamviewer");
+  const snapshot = await getDoc(docRef);
+  
+  if (snapshot.exists()) {
+    const data = snapshot.data();
+    console.log({ observeList: data.observeList });
+    return data.observeList;
+  } else {
+    console.error("Document not found");
+    return [];
+  }
+}
